81ccc1f47f2508b5fe4e5e89ebabe044546c0465
AndreaDuh <xAsianRejectx@gmail.com>
2015-10-13 16:58:07 -0700

    Made changes to project

---
+++ js/app.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+        $('.ryu').mouseenter(function(){
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+    })
+    .mouseleave(function() {
+        $('.ryu-still').show();
+        $('.ryu-ready').hide();
+    })
+    .mousedown(function(){
+        playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate(
+            {'left': '1020px'},
+            500,
+            function(){
+                $(this).hide();
+                $(this).css('left', '520px');
+            });
+    })
+    .mouseup(function(){
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    });
+    
+    .keydown(function(){
+        playGuile();
+        $('.ryu-ready').hide();
+        $('.ryu-cool').show();
+    });
+
+});
+
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+
+function playGuile() {
+    $('#guile-theme')[0].volume = 0.5;
+    $('#guile-theme')[0].load();
+    $('#guile-theme')[0].play();
+}
