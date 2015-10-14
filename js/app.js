68b199619ec646cf1657b617feb2205973a38a31
AndreaDuh <xAsianRejectx@gmail.com>
2015-10-14 04:52:36 -0700

    I don't know

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
     .mouseleave(function() {
         $('.ryu-still').show();
         $('.ryu-ready').hide();
+        $('.ryu-throwing').hide();
     })
     .mousedown(function(){
         playHadouken();
@@ -17,28 +18,37 @@ $(document).ready(function(){
             function(){
                 $(this).hide();
                 $(this).css('left', '520px');
-            });
+            })
     })
     .mouseup(function(){
         $('.ryu-throwing').hide();
         $('.ryu-ready').show();
-    });
-    
-    .keydown(function(){
-        playGuile();
-        $('.ryu-ready').hide();
-        $('.ryu-cool').show();
-    });
-
+    })
 });
 
-function playHadouken() {
-    $('#hadouken-sound')[0].volume = 0.5;
-    $('#hadouken-sound')[0].load();
-    $('#hadouken-sound')[0].play();
+$('html').on('keydown', function(event) {
+        if (event.which == 88) {
+            playGuile();
+            $('.ryu-still').hide();
+            $('.ryu-ready').hide();
+            $('.ryu-cool').show();
+        }
+    })
+       .on('keyup', function(event) {
+        if (event.which == 88) {
+            $('.ryu-cool').hide();
+            $('.ryu-still').show();
+        }
+});
 
 function playGuile() {
     $('#guile-theme')[0].volume = 0.5;
     $('#guile-theme')[0].load();
     $('#guile-theme')[0].play();
+}    
+
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
 }
